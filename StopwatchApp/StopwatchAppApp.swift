f0e77542f08e7d31e4f57fff8ffa91c7d7dcc5ab
Hayden Chan <hchan@lcs.on.ca>
2023-11-02 08:34:51 -0400

    Initial Commit

---
+++ StopwatchApp/StopwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppApp.swift
+//  StopwatchApp
+//
+//  Created by Hayden Chan on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
