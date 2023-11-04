43d23494accf48c5f1305ebf530226232ca93878
Hayden Chan <hchan@lcs.on.ca>
2023-11-04 10:28:23 -0400

    finish stopwatch

--- StopwatchApp/ContentView.swift
+++ StopwatchApp/ContentView.swift
@@ -4,9 +4,7 @@
 //
 //  Created by Hayden Chan on 2023-11-02.
 //
-
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
         ZStack {
@@ -14,6 +12,7 @@ struct ContentView: View {
             Color.black
                 .ignoresSafeArea()
             VStack {
+                Spacer()
                 Text("00:09.96")
                     .font(Font.system(size:90,weight: .thin))
                     .foregroundColor(.white)
@@ -32,17 +31,61 @@ struct ContentView: View {
                         label: "Start",
                         labelColor: .green
                     )
-                
+                    
                     
                 }
+                List{
+                    Group{
+                        Text("1")
+                        Text("2")
+                        Text("3")
+                        Text("4")
+                        Text("5")
+                    }
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0,bottom: 0,trailing: 0))
+                }
+                .frame(height:300)
+                .listStyle(.plain)
+                    
                 
             }
             .padding()
         }
-     
     }
 }
 
 #Preview {
-    ContentView()
+    TabView(selection: Binding.constant(3)){
+    
+        
+        Text("World Clock")
+            .tabItem {
+                Image(systemName: "globe")
+                Text("World Clock")
+                
+            }
+            .tag(1)
+        Text("Alarm")
+            .tabItem {
+                Image(systemName: "alarm.fill")
+                Text("Alarm")
+                
+            }
+            .tag(2)
+        ContentView()
+            .tabItem {
+                Image(systemName: "stopwatch.fill")
+                Text("Stopwatch")
+            }
+            .tag(3)
+        Text("Timer")
+            .tabItem{
+                Image(systemName: "timer")
+                Text("Timer")
+            }
+            .tag(4)
+        
+    }
+    .accentColor(.orange)
+    .preferredColorScheme(.dark)
 }
